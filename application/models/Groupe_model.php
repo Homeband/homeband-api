ccd5b31128e4540d35bb1fe5ea352932dbb2fdfa
Nicolas Gérard <nicolas.gerard@proximus.be>
2017-12-17 23:19:19 +0100

    Suppression des champs du model groupes

--- application/models/Groupe_model.php
+++ application/models/Groupe_model.php
@@ -8,117 +8,133 @@
 
 class Groupe_model extends CI_Model
 {
-    public $biographie = '';
-    public $contacts = '';
-    public $email = '';
-    public $est_actif = 1;
-    public $id_groupes = 0;
-    public $id_style = 0;
-    public $id_villes = 0;
-    public $lien_bandcamp = '';
-    public $lien_facebook = '';
-    public $lien_instagram = '';
-    public $lien_itunes = '';
-    public $lien_soundcloud = '';
-    public $lien_spotify = '';
-    public $lien_twitter = '';
-    public $lien_youtube = '';
-    public $login = '';
-    public $mot_de_passe = '';
-    public $nom = '';
+    private $table = 'GROUPES';
 
-    public function inscrire(){
-        $data = get_object_vars($this);
-        return $this->db->insert('groupes', $data);
+    public function inscrire($groupe){
+        return $this->db->insert('groupes', $groupe);
     }
-    public function connecter(){
+
+    public function connecter($groupe){
         // requête de type where 'login' = 'Chris'
-        $this->db->where('login', $this->login);
-        $this->db->where('mot_de_passe', $this->mot_de_passe);
+        $this->db->where('login', $groupe->login);
+        //$this->db->where('mot_de_passe', $this->mot_de_passe);
         $this->db->where('est_actif', TRUE);
         // Select * from
         $query = $this->db->get("GROUPES");
         //selectionne la première ligne
-        $row = $query->row(0, 'Groupe_model');
+        $row = $query->row(0, 'Groupe');
 
         // Si variable row = à quelque chose
-        if(isset($row)) {
+        if(isset($row) && $row->check_password($groupe->mot_de_passe)) {
             // Connexion réussie
-            foreach(get_object_vars($row) as $key => $value){
-                if($key != 'mot_de_passe'){
-                    $this->$key = $value;
-                }
-            }
 
-            $this->mot_de_passe = '';
+            $row->mot_de_passe = '';
 
             //Objet courant va comprendre tout ça donc $user dans controller Welcome sera = à ça
-            return TRUE;
+            return $row;
 
         } else{
             // Echec de la connexion
 
-            return FALSE;
+            return NULL;
         }
     }
 
-    public function lister($cp,$rayon,$styles){
-        // Sélectionne la ville si le champs cp est remplis
+    /**
+     * Recherche une liste de groupes sur certains critères
+     * @param $cp
+     * @param $rayon
+     * @param $styles
+     * @return mixed
+     */
+    public function lister($cp, $rayon, $styles){
+        // Sélection de tous les champs de la table groupes [est_actif = 1]
         $this->db->select('groupes.*');
         $this->db->from('groupes');
+        $this->db->where('groupes.est_actif', true);
+
+        // Filtrage sur le code postal
         if(isset($cp)){
             $this->db->join('villes', 'villes.id_villes = groupes.id_villes');
             $this->db->where('villes.code_postal' ,$cp);
 
+            // Filtrage sur le rayon
             if(isset($rayon)){
 
             }
         }
 
+        // Filtrage sur le style de musique
         if(isset($styles)){
             $this->db->join('styles', 'styles.id_styles = groupes.id_styles');
             $this->db->where('styles.id_styles' ,$styles);
         }
+
+
         // Récupère tout les champs de la table 'groupes' et renvoie la liste
         $query = $this->db->get();
         return $query->result();
 
     }
+
+    /**
+     * Récupère un groupe en fonction de son ID
+     * @param $id_groupe
+     * @return mixed
+     */
     public function recuperer($id_groupe){
-        $this->db->select('groupes.*');
+        // Préparation de la requête
         $this->db->from('groupes');
-        $this->db->where('id_groupes' ,$id_groupe);
+        $this->db->where('id_groupes', $id_groupe);
+        $this->db->where('est_actif', true);
+
+        // Execution de la requête
         $query = $this->db->get();
-        $row = $query->row(0, 'Groupe_model');
+
+        // Récupération et renvoi de la première ligne
+        $row = $query->row(0, 'Groupe');
         return $row;
     }
-    public function modifier($group_put){
-        $this->db->where('id_groupes' ,$group_put->id_groupes);
-        return $this->db->update('groupes', $group_put);
+
+    /**
+     * Modifie la fiche d'un groupe
+     * @param $groupe
+     * @return mixed
+     */
+    public function modifier($groupe){
+        // Préparation de la requête
+        $this->db->where('id_groupes' ,$groupe->id_groupes);
+
+        // Modification de la fiche
+        return $this->db->update('groupes', $groupe);
     }
+
+    /**
+     * Désactive la fiche d'un groupe
+     * @param $id_groupe
+     * @return mixed
+     */
     public function supprimer($id_groupe){
+        // Préparation de la requête
+        $this->db->from('groupes');
+
+        // Modification du statut est_actif à false
+        $this->db->set('est_actif', false);
         $this->db->where('id_groupes' ,$id_groupe);
-        return $this->db->delete('groupes');
+
+        return $this->db->update();
     }
 
+    /**
+     * Vérifie la disponibilité du login
+     * @param $login
+     * @return bool
+     */
+    public function verifie_login($login){
+        $this->db->from($this->table);
+        $this->db->where('login', $login);
+        $this->db->where('est_actif', true);
 
-    public function recupererDetails_membres($id_groupe,$id_membres){
-        $this->db->select('membres_groupes.*');
-        $this->db->from('membres_groupes');
-        $this->db->where('id_groupes' ,$id_groupe);
-        $this->db->where ('id_membres',$id_membres);
-        $query = $this->db->get();
-        $row = $query->row(0, 'Groupe_model');
-        return $row;
-    }
-    public function modifierDetails($membres_put){
-        $this->db->where('id_groupes' ,$membres_put->id_groupes);
-        return $this->db->update('membres_groupes', $membres_put);
-    }
-
-    public function supprimerDetails($id_groupe,$id_membres){
-        $this->db->where('id_groupes' ,$id_groupe);
-        $this->db->where('id_membres' ,$id_membres);
-        return $this->db->delete('membres_groupes');
+        return ($this->db->count_all_results() == 0);
     }
 }
