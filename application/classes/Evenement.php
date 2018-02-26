dc404960816469d8bea3199cf5d2b2de5729444e
Nicolas Gérard <nicolas.gerard@proximus.be>
2018-02-26 20:45:21 +0100

    Mise à jour des classes d'objets

--- application/classes/Evenement.php
+++ application/classes/Evenement.php
@@ -5,6 +5,7 @@ class Evenement extends MY_Object
     public $id_evenements = 0;
     public $nom = '';
     public $description = '';
+    public $date_maj = '';
     public $est_actif = true;
     public $id_groupes = 0;
 }
--- application/classes/EvenementDetail.php
+++ application/classes/EvenementDetail.php
@@ -7,5 +7,7 @@ class EvenementDetail extends MY_Object
     public $prix = 0.0;
     public $est_description_perso = false;
     public $description = '';
+    public $lien_facebook = '';
+    public $date_maj = '';
     public $id_adresses = 0;
 }
