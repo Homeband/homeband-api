fa2ed4de37f45b019846b85cfc0eaca6c8a990cd
Nicolas <nicolas.gerard@std.heh.be>
2018-03-05 09:16:04 +0100

    Modificiation du message d'erreur renvoyé lors des exceptions

--- application/core/MY_Exceptions.php
+++ application/core/MY_Exceptions.php
@@ -11,6 +11,6 @@ class MY_Exceptions extends CI_Exceptions
     function show_error($heading, $message, $template = 'error_general', $status_code = 500)
     {
         log_message( 'debug', print_r( $message, TRUE ) );
-        throw new Exception(is_array($message) ? $message[1] : $message, $status_code );
+        throw new Exception(is_array($message) ? $message[0] : $message, $status_code );
     }
 }
